3affab089ee7db833e2292044e95737e5e6351c5
Herbert Hudson <herberthudson@gmail.com>
2023-07-09 15:35:31 -0300

    add astrocommunity sonokai colorscheme

--- nvim/lua/user/plugins/community.lua
+++ nvim/lua/user/plugins/community.lua
@@ -2,5 +2,10 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.git.git-blame-nvim" },
   { import = "astrocommunity.git.openingh-nvim" },
-  { import = "astrocommunity.editing-support.vim-move" }
+  { import = "astrocommunity.editing-support.vim-move" },
+  { import = "astrocommunity.colorscheme.sonokai" },
+  {
+    "sonokai",
+    event = "BufRead",
+  }
 }
