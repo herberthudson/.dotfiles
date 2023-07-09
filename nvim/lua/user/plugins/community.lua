7274bd21b0d01f20ae811998f55aebe47e3f7bf2
Herbert Hudson <herberthudson@gmail.com>
2023-07-09 14:43:14 -0300

    adding vim-move plugin

--- nvim/lua/user/plugins/community.lua
+++ nvim/lua/user/plugins/community.lua
@@ -1,5 +1,6 @@
 return {
   "AstroNvim/astrocommunity",
-  { import = "astrocommunity.git.git-blame-nvim", enable = true },
+  { import = "astrocommunity.git.git-blame-nvim" },
   { import = "astrocommunity.git.openingh-nvim" },
+  { import = "astrocommunity.editing-support.vim-move" }
 }
