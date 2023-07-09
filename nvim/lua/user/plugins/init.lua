6accf6b233b333c7179cac50c8a77a07c3948c1a
Herbert Hudson <herberthudson@gmail.com>
2023-07-09 19:15:49 -0300

    update gitsings plugin

--- nvim/lua/user/plugins/init.lua
+++ nvim/lua/user/plugins/init.lua
@@ -1,13 +1,34 @@
 return {
-  {
-    "mbbill/undotree",
-    cmd = "UndotreeToggle",
-    config = function()
-      vim.g.undotree_SetFocusWhenToggle = 1
-    end
-  },
-  {
-    "ThePrimeagen/vim-be-good",
-    cmd = "VimBeGood",
-  }
+	{
+		"mbbill/undotree",
+		cmd = "UndotreeToggle",
+		config = function()
+			vim.g.undotree_SetFocusWhenToggle = 1
+		end,
+	},
+	{
+		"ThePrimeagen/vim-be-good",
+		cmd = "VimBeGood",
+	},
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup({
+				signs = {
+					add = {
+						text = "+",
+					},
+					change = {
+						text = "±",
+					},
+					changedelete = {
+						text = "⨦",
+					},
+					delete = {
+						text = "✖",
+					},
+				},
+			})
+		end,
+	},
 }
