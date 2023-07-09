95cc54117af7398bcfeb6dd439665d7cd0ce105e
Herbert Hudson <herberthudson@gmail.com>
2023-07-09 20:15:06 -0300

    update statusline config

--- nvim/lua/user/plugins/init.lua
+++ nvim/lua/user/plugins/init.lua
@@ -31,4 +31,41 @@ return {
 			})
 		end,
 	},
+	{
+		"rebelot/heirline.nvim",
+		opts = function(_, opts)
+			local status = require("astronvim.utils.status")
+			opts.statusline = {
+				hl = { fg = "fg", bg = "bg" },
+				status.component.mode({
+					mode_text = {
+						n = "NORMAL",
+						i = "INSERT",
+						v = "VISUAL",
+						V = "V-LINE",
+						c = "COMMAND",
+						s = "SELECT",
+						S = "S-LINE",
+						[""] = "SELECT BLOCK",
+						t = "TERMINAL",
+						padding = {
+							left = 1,
+							right = 1,
+						},
+					},
+				}),
+				status.component.git_branch(),
+				status.component.file_info({ filetype = {}, filename = false, file_modified = false }),
+				status.component.git_diff(),
+				status.component.diagnostics(),
+				status.component.fill(),
+				status.component.cmd_info(),
+				status.component.fill(),
+				status.component.lsp(),
+				status.component.treesitter(),
+				status.component.nav(),
+			}
+			return opts
+		end,
+	},
 }
